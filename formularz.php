ed538252a36f634fc2647a584944b8005e3a5936
Adamskastudio <151088111+Adamskastudio@users.noreply.github.com>
2023-11-16 22:32:27 +0100

    Add files via upload

--- formularz.php
+++ formularz.php
@@ -1,18 +1,22 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
     $imie = $_POST["imie"];
     $nazwisko = $_POST["nazwisko"];
     $mail = $_POST["mail"];
     $wiadomosc = $_POST["wiadomosc"];
-
     
-    $to = "lorak.stawecki@onet.pl";
+    $to = "endlessstudio.pl";
     $subject = "Nowa wiadomość od $imie $nazwisko";
     $message = "Wiadomość od: $imie $nazwisko\nEmail: $mail\nWiadomość:\n$wiadomosc";
     $headers = "From: $mail";
 
-    mail($to, $subject, $message, $headers);
-    echo "Dziękujemy za wysłanie wiadomości!";
+   
+    $mail_sent = mail($to, $subject, $message, $headers);
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<p>Dziękujemy za wysłanie wiadomości!</p>";
+    } else {
+        echo "<p>Wystąpił problem podczas wysyłania wiadomości.</p>";
+    }
 }
 ?>
